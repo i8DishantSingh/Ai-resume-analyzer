ae5ce4ea51237277a0f2d01fe688c27a549b20b7
i8DishantSingh <professional.id.dishant@gmail.com>
2026-05-07 13:40:32 +0530

    minor changes

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,8 +1,7 @@
-import ResumeCard from "~/components/resumeCard";
+import ResumeCard from "~/components/ResumeCard";
 import { resumes } from "../../constants";
 import type { Route } from "./+types/home";
 import Navbar from "~/components/Navbar";
-import { resume } from "react-dom/server";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -16,18 +15,19 @@ export default function Home() {
     <main className="bg-[url('/images/bg-main.svg')] bg-cover">
       <Navbar></Navbar>
       <section className="main-section">
-        <div className="page-heading">
-          <h1>Track your applications & ratings</h1>
+        <div className="page-heading py-5">
+          <h1>Track Your Applications & Resume Ratings</h1>
           <h2>Review your submissions and check AI-powered feedback.</h2>
         </div>
+
+        {resumes.length > 0 && (
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10">
+            {resumes.map((resume) => (
+              <ResumeCard key={resume.id} resume={resume} />
+            ))}
+          </div>
+        )}
       </section>
-      {resumes.length > 0 && (
-        <div>
-          {resumes.map((resume) => (
-            <ResumeCard key={resume.id} resume={resume} />
-          ))}
-        </div>
-      )}
     </main>
   );
 }
